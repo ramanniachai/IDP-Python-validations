33abbb0a4b22dd845f69cf554ccf0602593c95a1
ramanniachai <raman_niachai.com>
2025-01-10 09:21:44 +0300

    Update Python scripts directory

--- Python scripts/price_validation.py
+++ Python scripts/price_validation.py
@@ -20,7 +20,7 @@ def get_json_from_api(url):
         if response.status_code == 200:
             data = json.loads(response.content.decode('utf-8'))
         else:
-            print(f"Unable to retrieve data from API, status cide is {response.status_code}")
+            print(f"Unable to retrieve data from API, status code is {response.status_code}")
             data = None
     except requests.exceptions.RequestException as e:
         print(f"Error while sending API request: {e}")
@@ -52,10 +52,11 @@ def price_in_newProducts(api, location):
     for size_values in api['sizeGroups'].values():
         size_id_to_name[size_values['id']] = size_values['name'] 
 
-    with open('Raw Prices 01092025.txt', 'a') as f:
+    with open('Raw Prices 01102025.txt', 'a') as f:
         all_items_at_this_location = []
         last_updated_timestamp = api.get('lastUpdatedTimestamp')
         current_utc_time = datetime.utcnow()
+        item_to_check = ['idp-sdi-itm-81730-000', 'idp-sdi-itm-82770-81730-000', 'idp-sdi-itm-81750-000', 'idp-sdi-itm-82780-81750-000', 'idp-sdi-itm-82800-000', 'idp-sdi-itm-82803-000', 'idp-sdi-itm-82804-000', 'idp-sdi-itm-82121-000', 'idp-sdi-itm-82120-000', 'idp-sdi-itm-82122-000', 'idp-sdi-itm-82123-000', 'idp-sdi-itm-82124-000']
         for values in api['products'].values():
             product_type = values.get('type')
             for item_keys, item_values in values['items'].items():
@@ -63,197 +64,39 @@ def price_in_newProducts(api, location):
                 item_id = item_values["id"]
                 item_size = item_values.get('sizeGroupId')
                 item_group = item_values.get('itemGroupId')
-                if item_id == "idp-sdi-itm-81730-000":
-                    item_price = item_values.get('price')
 
-                    if item_price:
-                        print(f"{current_utc_time} | {location} | {items_name} | {item_price.get('currentPrice')}")
-                        result = f"{current_utc_time} | {location} | {items_name} | {item_price.get('currentPrice')}\n"
-                        f.write(result)
-                    if item_price == {}:
-                        print(f"{current_utc_time} | {location} | {items_name} | Empty {{}}")
-                        result = f"{current_utc_time} | {location} | {items_name} | Empty{{}}\n"
-                        f.write(result)
-                    elif not item_price and item_price != {}:
-                        print(f"{current_utc_time} | {location} | {items_name} | No price")
-                        result = f"{current_utc_time} | {location} | {items_name} | No price\n"
-                        f.write(result)
+                for i in item_to_check:
+                    if item_id == i and item_size == "idp-sdi-sig-999-999":
+                        item_price = item_values.get('price')
 
-                if item_id == "idp-sdi-itm-82770-81730-000":
-                    item_price = item_values.get('price')
+                        if item_price:
+                            print(f"{current_utc_time} | {location} | {items_name} | {item_price.get('currentPrice')}")
+                            result = f"{current_utc_time} | {location} | {items_name} | {item_price.get('currentPrice')}\n"
+                            f.write(result)
+                        if item_price == {}:
+                            print(f"{current_utc_time} | {location} | {items_name} | Empty {{}}")
+                            result = f"{current_utc_time} | {location} | {items_name} | Empty{{}}\n"
+                            f.write(result)
+                        elif not item_price and item_price != {}:
+                            print(f"{current_utc_time} | {location} | {items_name} | No price")
+                            result = f"{current_utc_time} | {location} | {items_name} | No price\n"
+                            f.write(result)
 
-                    if item_price:
-                        print(f"{current_utc_time} | {location} | {items_name} | {item_price.get('currentPrice')}")
-                        result = f"{current_utc_time} | {location} | {items_name} | {item_price.get('currentPrice')}\n"
-                        f.write(result)
-                    if item_price == {}:
-                        print(f"{current_utc_time} | {location} | {items_name} | Empty {{}}")
-                        result = f"{current_utc_time} | {location} | {items_name} | Empty{{}}\n"
-                        f.write(result)
-                    elif not item_price and item_price != {}:
-                        print(f"{current_utc_time} | {location} | {items_name} | No price")
-                        result = f"{current_utc_time} | {location} | {items_name} | No price\n"
-                        f.write(result)
-                    
-                if item_id == "idp-sdi-itm-81750-000":
-                    item_price = item_values.get('price')
+                    if item_id == i and item_size != "idp-sdi-sig-999-999":
+                        item_price = item_values.get('price')
 
-                    if item_price:
-                        print(f"{current_utc_time} | {location} | {items_name} | {item_price.get('currentPrice')}")
-                        result = f"{current_utc_time} | {location} | {items_name} | {item_price.get('currentPrice')}\n"
-                        f.write(result)
-                    if item_price == {}:
-                        print(f"{current_utc_time} | {location} | {items_name} | Empty {{}}")
-                        result = f"{current_utc_time} | {location} | {items_name} | Empty{{}}\n"
-                        f.write(result)
-                    elif not item_price and item_price != {}:
-                        print(f"{current_utc_time} | {location} | {items_name} | No price")
-                        result = f"{current_utc_time} | {location} | {items_name} | No price\n"
-                        f.write(result)
-
-                if item_id == "idp-sdi-itm-82780-81750-000":
-                    item_price = item_values.get('price')
-
-                    if item_price:
-                        print(f"{current_utc_time} | {location} | {items_name} | {item_price.get('currentPrice')}")
-                        result = f"{current_utc_time} | {location} | {items_name} | {item_price.get('currentPrice')}\n"
-                        f.write(result)
-                    if item_price == {}:
-                        print(f"{current_utc_time} | {location} | {items_name} | Empty {{}}")
-                        result = f"{current_utc_time} | {location} | {items_name} | Empty{{}}\n"
-                        f.write(result)
-                    elif not item_price and item_price != {}:
-                        print(f"{current_utc_time} | {location} | {items_name} | No price")
-                        result = f"{current_utc_time} | {location} | {items_name} | No price\n"
-                        f.write(result)
-
-                if item_id == "idp-sdi-itm-82800-000":
-                    item_price = item_values.get('price')
-
-                    if item_price:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | {item_price.get('currentPrice')}")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | {item_price.get('currentPrice')}\n"
-                        f.write(result)
-                    if item_price == {}:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | Empty {{}}")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | Empty{{}}\n"
-                        f.write(result)
-                    elif not item_price and item_price != {}:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | No price")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | No price\n"
-                        f.write(result)
-
-                if item_id == "idp-sdi-itm-82803-000":
-                    item_price = item_values.get('price')
-
-                    if item_price:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | {item_price.get('currentPrice')}")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | {item_price.get('currentPrice')}\n"
-                        f.write(result)
-                    if item_price == {}:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | Empty {{}}")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | Empty{{}}\n"
-                        f.write(result)
-                    elif not item_price and item_price != {}:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | No price")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | No price\n"
-                        f.write(result)
-
-                if item_id == "idp-sdi-itm-82804-000":
-                    item_price = item_values.get('price')
-
-                    if item_price:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | {item_price.get('currentPrice')}")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | {item_price.get('currentPrice')}\n"
-                        f.write(result)
-                    if item_price == {}:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | Empty {{}}")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | Empty{{}}\n"
-                        f.write(result)
-                    elif not item_price and item_price != {}:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | No price")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | No price\n"
-                        f.write(result)
-
-                if item_id == "idp-sdi-itm-82121-000":
-                    item_price = item_values.get('price')
-
-                    if item_price:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | {item_price.get('currentPrice')}")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | {item_price.get('currentPrice')}\n"
-                        f.write(result)
-                    if item_price == {}:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | Empty {{}}")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | Empty{{}}\n"
-                        f.write(result)
-                    elif not item_price and item_price != {}:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | No price")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | No price\n"
-                        f.write(result)
-
-                if item_id == "idp-sdi-itm-82122-000":
-                    item_price = item_values.get('price')
-
-                    if item_price:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | {item_price.get('currentPrice')}")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | {item_price.get('currentPrice')}\n"
-                        f.write(result)
-                    if item_price == {}:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | Empty {{}}")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | Empty{{}}\n"
-                        f.write(result)
-                    elif not item_price and item_price != {}:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | No price")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | No price\n"
-                        f.write(result)
-
-                if item_id == "idp-sdi-itm-82120-000":
-                    item_price = item_values.get('price')
-
-                    if item_price:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | {item_price.get('currentPrice')}")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | {item_price.get('currentPrice')}\n"
-                        f.write(result)
-                    if item_price == {}:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | Empty {{}}")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | Empty{{}}\n"
-                        f.write(result)
-                    elif not item_price and item_price != {}:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | No price")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | No price\n"
-                        f.write(result)
-
-                if item_id == "idp-sdi-itm-82123-000":
-                    item_price = item_values.get('price')
-
-                    if item_price:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | {item_price.get('currentPrice')}")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | {item_price.get('currentPrice')}\n"
-                        f.write(result)
-                    if item_price == {}:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | Empty {{}}")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | Empty{{}}\n"
-                        f.write(result)
-                    elif not item_price and item_price != {}:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | No price")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | No price\n"
-                        f.write(result)
-
-                if item_id == "idp-sdi-itm-82124-000":
-                    item_price = item_values.get('price')
-
-                    if item_price:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | {item_price.get('currentPrice')}")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | {item_price.get('currentPrice')}\n"
-                        f.write(result)
-                    if item_price == {}:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | Empty {{}}")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | Empty{{}}\n"
-                        f.write(result)
-                    elif not item_price and item_price != {}:
-                        print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | No price")
-                        result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | No price\n"
-                        f.write(result)
+                        if item_price:
+                            print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | {item_price.get('currentPrice')}")
+                            result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | {item_price.get('currentPrice')}\n"
+                            f.write(result)
+                        if item_price == {}:
+                            print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | Empty {{}}")
+                            result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | Empty{{}}\n"
+                            f.write(result)
+                        elif not item_price and item_price != {}:
+                            print(f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | No price")
+                            result = f"{current_utc_time} | {location} | {items_name} {size_id_to_name[item_size]} | No price\n"
+                            f.write(result)
 
 
 data_list = []
