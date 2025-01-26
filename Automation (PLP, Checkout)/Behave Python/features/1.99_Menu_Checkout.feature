c48dd1833ac1e274751d86bca6315c3799ff0741
ramanniachai <raman_niachai.com>
2025-01-26 11:17:23 +0300

    Update IDP-Python-validations repository

---
+++ Automation (PLP, Checkout)/Behave Python/features/1.99_Menu_Checkout.feature
@@ -0,0 +1,26 @@
+Feature: Add all products to the bag and proceed to checkout
+
+    Background:
+        Given Sonic web page is opened_M
+
+    Scenario Outline: Add a $1.99 Menu to the bag and proceed to checkout
+        When I click on the "$1.99 Menu" category
+        And I open the product at index <index>
+        And I add the product to the bag
+        And I proceed to checkout_M
+        Then I should be on the checkout page_M
+        And I click on the back button_M
+        And I remove the product from the bag
+        And I close the bag_M
+
+        Examples:
+            | index |
+            | 1     |
+            | 2     |
+            | 3     |
+            | 4     |
+            | 5     |
+            | 6     |
+            | 7     |
+            | 8     |
+            | 9     |
---
+++ Automation (PLP, Checkout)/Behave Python/features/steps/1.99_Menu_Checkout.py
@@ -0,0 +1,77 @@
+from behave import given, when, then
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time
+
+def before_all(context):
+    context.browser = webdriver.Chrome()
+    context.browser.maximize_window()
+
+def after_all(context):
+    context.browser.quit()
+
+@given('Sonic web page is opened_M')
+def step_given_on_menu_page(context):
+    #context.browser.get('https://www.sonicdrivein.com/?locationId=6273')
+    context.browser.get('https://cfsnc.uat.irb.digital/?locationId=8810')
+
+
+@when('I click on the "$1.99 Menu" category')
+def step_when_click_1_99_menu(context):
+    menu_selector = 'a[aria-labelledby="instructions-value-menu"]'
+    menu_element = WebDriverWait(context.browser, 20).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, menu_selector))
+    )
+    menu_element.click()
+
+@when('I open the product at index {index}')
+def step_when_open_product_at_index(context, index):
+    product_selector = f"div[data-gtm-id='productItem']:nth-child({index}) a"
+    product_element = WebDriverWait(context.browser, 20).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, product_selector))
+    )
+    product_element.click()
+
+@when('I add the product to the bag')
+def step_when_add_to_bag(context):
+    add_to_bag_button = WebDriverWait(context.browser, 20).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[text()='Add to Bag']"))
+    )
+    add_to_bag_button.click()
+
+@when('I proceed to checkout_M')
+def step_when_proceed_to_checkout(context):
+    checkout_button = WebDriverWait(context.browser, 20).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[text()='Checkout']"))
+    )
+    checkout_button.click()
+
+@then('I should be on the checkout page_M')
+def step_then_on_checkout_page(context):
+    WebDriverWait(context.browser, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".checkout_paymentHeaderContainer__1I3zU"))
+    )
+    assert 'checkout' in context.browser.current_url, "Unable to open the checkout page"
+
+@then('I click on the back button_M')
+def step_then_click_back_button(context):
+    back_button = WebDriverWait(context.browser, 20).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "a.paymentHeader_backContainer__IE_5k"))
+    )
+    back_button.click()
+
+@then('I remove the product from the bag')
+def step_then_remove_item_from_bag(context):
+    remove_button = WebDriverWait(context.browser, 20).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='remove bag item']"))
+    )
+    remove_button.click()
+
+@then('I close the bag_M')
+def step_then_close_bag(context):
+    close_button = WebDriverWait(context.browser, 20).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "button.bag_closeButton__F5CVN"))
+    )
+    close_button.click()
